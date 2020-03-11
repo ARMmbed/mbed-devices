e11f2c30793147a616c2bac41814d78d15f37e70
Bartek Mucha <muchzill4@gmail.com>
2020-03-11 08:48:18 +0000

    ♻ Refactor resolve target (#47)
    * Refactor resolve target
    
    * Add news file

--- tests/_internal/test_resolve_target.py
+++ tests/_internal/test_resolve_target.py
@@ -6,85 +6,97 @@ from mbed_targets import UnknownTarget
 from tests.factories import CandidateDeviceFactory
 from mbed_devices._internal.htm_file import OnlineId
 from mbed_devices._internal.resolve_target import (
+    UnableToBuildResolver,
     NoTargetForCandidate,
-    _resolve_target_using_file_contents,
+    _get_all_htm_files_contents,
     resolve_target,
+    _build_target_resolver,
 )
 
 
+@mock.patch("mbed_devices._internal.resolve_target._build_target_resolver")
 class TestResolveTarget(TestCase):
-    @mock.patch("mbed_devices._internal.resolve_target._resolve_target_using_file_contents")
-    def test_resolves_target_using_htm_file_contents_found_in_mount_points(self, _resolve_target_using_file_contents):
-        files = [(pathlib.Path("/test-1/mbed.htm"), "foo"), (pathlib.Path("/test-2/whatever.htm"), "bar")]
-        with Patcher() as patcher:
-            for (path, contents) in files:
-                patcher.fs.create_file(str(path), contents=contents)
-            patcher.fs.create_file("/test-1/._MBED.HTM", contents="hello")
-            patcher.fs.create_file("/test-1/file.txt", contents="whatever")
+    def test_returns_target_resolved_using_built_target_resolver(self, _build_target_resolver):
+        candidate = CandidateDeviceFactory()
 
-            result = resolve_target(
-                CandidateDeviceFactory(mount_points=[pathlib.Path("/test-1"), pathlib.Path("/test-2")])
-            )
+        subject = resolve_target(candidate)
 
-        self.assertEqual(result, _resolve_target_using_file_contents.return_value)
-        _resolve_target_using_file_contents.assert_called_once_with(["foo", "bar"])
+        self.assertEqual(subject, _build_target_resolver.return_value.return_value)
+        _build_target_resolver.assert_called_once_with(candidate)
 
-
-class TestResolveTargetUsingFileContents(TestCase):
-    @mock.patch("mbed_devices._internal.resolve_target.read_product_code")
-    @mock.patch("mbed_devices._internal.resolve_target.get_target_by_product_code")
-    def test_resolves_targets_using_product_code_when_available(self, get_target_by_product_code, read_product_code):
-        read_product_code.return_value = "0123"
-
-        self.assertEqual(
-            _resolve_target_using_file_contents(["file contents"]), get_target_by_product_code.return_value
-        )
-        get_target_by_product_code.assert_called_once_with(read_product_code.return_value)
-        read_product_code.assert_called_once_with("file contents")
-
-    @mock.patch("mbed_devices._internal.resolve_target.read_product_code")
-    @mock.patch("mbed_devices._internal.resolve_target.get_target_by_product_code")
-    def test_raises_when_resolving_using_product_code_fails(self, get_target_by_product_code, read_product_code):
-        read_product_code.return_value = "1234"
-        get_target_by_product_code.side_effect = UnknownTarget
+    def test_raises_when_resolver_cannot_be_built(self, _build_target_resolver):
+        _build_target_resolver.side_effect = UnableToBuildResolver
+        candidate = CandidateDeviceFactory()
 
         with self.assertRaises(NoTargetForCandidate):
-            _resolve_target_using_file_contents(["some file contents"])
+            resolve_target(candidate)
+
+    def test_raises_when_target_cannot_be_resolved(self, _build_target_resolver):
+        resolver = mock.Mock(side_effect=UnknownTarget)
+        _build_target_resolver.return_value = resolver
+        candidate = CandidateDeviceFactory()
+
+        with self.assertRaises(NoTargetForCandidate):
+            resolve_target(candidate)
+
+
+@mock.patch("mbed_devices._internal.resolve_target._get_all_htm_files_contents")
+class TestBuildTargetResolver(TestCase):
+    @mock.patch("mbed_devices._internal.resolve_target.read_product_code")
+    @mock.patch("mbed_devices._internal.resolve_target.get_target_by_product_code")
+    def test_will_resolve_targets_using_product_code_when_available(
+        self, get_target_by_product_code, read_product_code, _get_all_htm_files_contents
+    ):
+        _get_all_htm_files_contents.return_value = ["file contents"]
+        read_product_code.return_value = "0123"
+        candidate = CandidateDeviceFactory()
+
+        subject = _build_target_resolver(candidate)()
+
+        self.assertEqual(subject, get_target_by_product_code.return_value)
+        get_target_by_product_code.assert_called_once_with(read_product_code.return_value)
+        read_product_code.assert_called_once_with("file contents")
+        _get_all_htm_files_contents.assert_called_once_with(candidate.mount_points)
 
     @mock.patch("mbed_devices._internal.resolve_target.read_product_code")
     @mock.patch("mbed_devices._internal.resolve_target.read_online_id")
     @mock.patch("mbed_devices._internal.resolve_target.get_target_by_online_id")
-    def test_resolves_targets_using_online_id_when_available(
-        self, get_target_by_online_id, read_online_id, read_product_code
+    def test_will_resolve_targets_using_online_id_when_available(
+        self, get_target_by_online_id, read_online_id, read_product_code, _get_all_htm_files_contents
     ):
+        _get_all_htm_files_contents.return_value = ["some file contents"]
         online_id = OnlineId(device_type="hat", device_slug="boat")
         read_product_code.return_value = None
         read_online_id.return_value = online_id
+        candidate = CandidateDeviceFactory()
 
-        self.assertEqual(
-            _resolve_target_using_file_contents(["some file contents"]), get_target_by_online_id.return_value
-        )
+        subject = _build_target_resolver(candidate)()
+
+        self.assertEqual(subject, get_target_by_online_id.return_value)
         read_online_id.assert_called_with("some file contents")
         get_target_by_online_id.assert_called_once_with(slug=online_id.device_slug, target_type=online_id.device_type)
 
     @mock.patch("mbed_devices._internal.resolve_target.read_product_code")
     @mock.patch("mbed_devices._internal.resolve_target.read_online_id")
-    @mock.patch("mbed_devices._internal.resolve_target.get_target_by_online_id")
-    def test_raises_when_resolving_using_online_id_fails(
-        self, get_target_by_online_id, read_online_id, read_product_code
+    def test_raises_when_no_information_found_on_candidate(
+        self, read_product_code, read_online_id, _get_all_htm_files_contents
     ):
-        read_product_code.return_value = None
-        read_online_id.return_value = OnlineId(device_type="hat", device_slug="boat")
-        get_target_by_online_id.side_effect = UnknownTarget
-
-        with self.assertRaises(NoTargetForCandidate):
-            _resolve_target_using_file_contents(["whatever"])
-
-    @mock.patch("mbed_devices._internal.resolve_target.read_product_code")
-    @mock.patch("mbed_devices._internal.resolve_target.read_online_id")
-    def test_raises_when_no_information_found_on_candidate(self, read_product_code, read_online_id):
+        _get_all_htm_files_contents.return_value = ["foo"]
         read_product_code.return_value = None
         read_online_id.return_value = None
 
-        with self.assertRaises(NoTargetForCandidate):
-            _resolve_target_using_file_contents(["zero information in this file"])
+        with self.assertRaises(UnableToBuildResolver):
+            _build_target_resolver(CandidateDeviceFactory())()
+
+
+class TestGetAllHtmFilesContents(TestCase):
+    def test_returns_contents_of_all_htm_files_in_given_directories(self):
+        with Patcher() as patcher:
+            patcher.fs.create_file("/test-1/mbed.htm", contents="foo")
+            patcher.fs.create_file("/test-2/whatever.htm", contents="bar")
+            patcher.fs.create_file("/test-1/file.txt", contents="txt files should not be read")
+            patcher.fs.create_file("/test-1/._MBED.HTM", contents="hidden files should not be read")
+
+            result = _get_all_htm_files_contents([pathlib.Path("/test-1"), pathlib.Path("/test-2")])
+
+        self.assertEqual(result, ["foo", "bar"])
