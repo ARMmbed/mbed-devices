6d1107ef96963f32a8db8ef838f9790c8bf04cb3
Bartek Mucha <muchzill4@gmail.com>
2020-03-17 14:29:36 +0000

    Proxy expose config variables from mbed-targets

--- mbed_devices/mbed_tools.py
+++ mbed_devices/mbed_tools.py
@@ -1,4 +1,6 @@
 """Integration with https://github.com/ARMmbed/mbed-tools."""
 from mbed_devices._internal.mbed_tools.list_connected_devices import list_connected_devices
+from mbed_targets.mbed_tools.config_variables import config_variables as mbed_targets_config_variables
 
 cli = list_connected_devices
+config_variables = mbed_targets_config_variables
